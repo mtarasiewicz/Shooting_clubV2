49c492993f9c4bbd10554a5da9df93f9db65d9f0
adrian <adrian@localhost>
2023-06-06 19:09:13 +0200

    Repair after last merging

--- sc/app/Models/Tournament.php
+++ sc/app/Models/Tournament.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Competition;
+use App\Models\CompetitionTournament;
 use Illuminate\Contracts\Cache\Store;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
@@ -39,6 +40,11 @@ class Tournament extends Model
         return $this->belongsToMany(User::class);
     }
 
+    public function competitionTournament()
+    {
+        return $this->belongsTo(CompetitionTournament::class);
+    }
+
     public function pdfUrl() : string
     {
         return $this-> pdfExists() 
@@ -62,49 +68,5 @@ class Tournament extends Model
     {
         return $this->rules !== null
         && Storage::disk('public')->exists($this->rules);
-    }
-}
-<?php
-
-namespace App\Models;
-
-use App\Models\User;
-use App\Models\Competition;
-use App\Models\CompetitionTournament;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-
-class Tournament extends Model
-{
-    use HasFactory;
-    use SoftDeletes;
-
-    protected $fillable = [
-        'name',
-        'date',
-        'venue',
-        'competitions',
-        'description'
-    ];
-
-    public function __toString()
-    {
-        return $this->name;
-    }
-
-    public function competitions()
-    {
-        return $this->belongsToMany(Competition::class);
-    }
-
-    public function participants()
-    {
-        return $this->belongsToMany(User::class);
-    }
-
-    public function competitionTournament()
-    {
-        return $this->belongsTo(CompetitionTournament::class);
     }
 }
